06a6a4937eddc7209cb0635cb3f368ea84d43418
guilhermePaciulli <gpaciulli100@gmail.com>
2017-05-16 17:53:13 -0300

    Initial commit

---
+++ UIDynamicTest/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  UIDynamicTest
+//
+//  Created by Guilherme Paciulli on 16/05/17.
+//  Copyright © 2017 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    
+    var square: UIView!
+    var snap: UISnapBehavior!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        self.square.backgroundColor = UIColor.gray
+        view.addSubview(self.square)
+        
+        let barrier = UIView(frame: CGRect(x: 0, y: 300, width: 130, height: 20))
+        barrier.backgroundColor = UIColor.red
+        view.addSubview(barrier)
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square])
+        
+        animator.addBehavior(gravity)
+        
+        collision = UICollisionBehavior(items: [square])
+        collision.addBoundary(withIdentifier: "barrier" as NSCopying, for: UIBezierPath(rect: barrier.frame))
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        collision.collisionDelegate = self
+        
+        let itemBehaviour = UIDynamicItemBehavior(items: [square])
+        itemBehaviour.elasticity = 0.6
+        animator.addBehavior(itemBehaviour)
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (snap != nil) {
+            animator.removeBehavior(snap)
+        }
+        
+        let touch = Array(touches)[0]
+        snap = UISnapBehavior(item: square, snapTo: touch.location(in: view))
+        animator.addBehavior(snap)
+    }
+    
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        let collidingView = item as! UIView
+        collidingView.backgroundColor = UIColor.yellow
+        UIView.animate(withDuration: 0.3) {
+            collidingView.backgroundColor = UIColor.gray
+        }
+    }
+}
+
